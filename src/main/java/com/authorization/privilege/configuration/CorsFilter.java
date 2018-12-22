635ee0c7c46c5602550a97875a3a98565ad743fc
1956025812@qq.com <1956025812@qq.com>
2018-12-22 16:33:13 +0800

    跨域配置 OK

---
+++ src/main/java/com/authorization/privilege/configuration/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.authorization.privilege.configuration;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author duxuebo
+ * @date 2018/12/1
+ * @description 全局跨域过滤器配置类
+ */
+@Component
+public class CorsFilter implements Filter {
+ 
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(req, res);
+    }
+ 
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+ 
+    @Override
+    public void destroy() {
+    }
+ 
+}
